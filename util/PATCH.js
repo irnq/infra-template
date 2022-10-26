6e5f0b2ba9fa60f074584295dd08792d77177d89
irnq <sk1m1gm@yandex.ru>
2022-10-26 14:12:54 +0400

    fix response

--- util/PATCH.js
+++ util/PATCH.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 console.log('Начинаем выполнять запрос в Трекер!');
 console.log('my phone number, ', process.env.ORG_ID);
+
+const patchBody = JSON.stringify({
+  summary: '*номер и дата релиза*',
+  description:
+    ' ответственный за релиз <имя того, кто запушил тег>\n коммиты, попавшие в релиз:\n <хеш коммита> <автор коммита> <описание коммита>\n <хеш коммита> <автор коммита> <описание коммита>]\n <хеш коммита> <автор коммита> <описание коммита>\n',
+});
+
 const options = {
   host: 'https://api.tracker.yandex.net',
   path: '/v2/issues/HOMEWORKSHRI-142',
@@ -10,15 +17,6 @@ const options = {
     OrgId: process.env.ORG_ID,
     Authorization: process.env.OAUTH_TOKEN,
     'Content-Type': 'application/json',
-  },
-  body: {
-    summary: '*номер и дата релиза*',
-    description: `
-    ответственный за релиз <имя того, кто запушил тег>\n
-    коммиты, попавшие в релиз:\n
-    <хеш коммита> <автор коммита> <описание коммита>\n
-    <хеш коммита> <автор коммита> <описание коммита>]\n
-    <хеш коммита> <автор коммита> <описание коммита>\n`,
   },
 };
 
@@ -33,4 +31,13 @@ const callback = function (response) {
   });
 };
 
-http.request(options, callback);
+let req = http.request(options, callback);
+
+req.on('error', (e) => {
+  console.error(e);
+});
+
+req.write(patchBody);
+req.end();
+
+console.log('данные успешно записаны в трекер!');
