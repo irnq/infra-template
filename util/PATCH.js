9a00018feca3d2bcd170041d17b93dd5fa14ee4e
irnq <sk1m1gm@yandex.ru>
2022-10-26 23:20:22 +0400

    add some vars

--- util/PATCH.js
+++ util/PATCH.js
@@ -3,8 +3,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 console.log('Начинаем выполнять запрос в Трекер!');
-console.log('------');
-console.log(github);
+console.log('------actor');
+console.log(github.context.actor);
+console.log('------tag');
+console.log(github.context.ref.split('/').at(-1));
+console.log('------commits');
+console.log(github.context.payload.commits);
 console.log('------ref-name');
 console.log(github.ref_name);
 console.log('------actor');
@@ -59,3 +63,37 @@ req.write(patchBody);
 req.end();
 
 console.log('данные успешно записаны в трекер!');
+/*
+{
+  context: Context {
+    payload: {
+      after: '2edddbbb962e7ccb7b9b21e0cd2bfbd883e4676a',
+      base_ref: null,
+      before: '0000000000000000000000000000000000000000',
+      commits: [],
+      compare: 'https://github.com/irnq/infra-template/compare/v0.0.4-3',
+      created: true,
+      deleted: false,
+      forced: false,
+      head_commit: [Object],
+      pusher: [Object],
+      ref: 'refs/tags/v0.0.4-3',
+      repository: [Object],
+      sender: [Object]
+    },
+    eventName: 'push',
+    sha: '642d6aa0520b7ed4ea3692e32feecf229e9018fe',
+    ref: 'refs/tags/v0.0.4-3',
+    workflow: 'test and build on release',
+    action: '__run_2',
+    actor: 'irnq',
+    job: 'test-and-build',
+    runNumber: 45,
+    runId: 3331864653,
+    apiUrl: 'https://api.github.com',
+    serverUrl: 'https://github.com',
+    graphqlUrl: 'https://api.github.com/graphql'
+  },
+  getOctokit: [Function: getOctokit]
+}
+*/
