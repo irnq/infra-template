761c8fd9795b7f7a2247f4f18f17e792a9eb19b4
irnq <sk1m1gm@yandex.ru>
2022-10-27 08:05:02 +0400

    добавил данные в шаблон

--- util/PATCH.js
+++ util/PATCH.js
@@ -1,33 +1,28 @@
 const http = require('https');
-const core = require('@actions/core');
 const github = require('@actions/github');
 
-console.log('Начинаем выполнять запрос в Трекер!');
-console.log('------actor');
-console.log(github.context.actor);
-console.log('------tag');
-console.log(github.context.ref.split('/').at(-1));
-console.log('------commits');
-console.log(github.context.payload.commits);
-console.log('------ref-name');
-console.log(github.ref_name);
-console.log('------actor');
-console.log(core.getInput('actor'));
-console.log('------commits');
-console.log(core.getInput('commits'));
-console.log('------version');
-console.log(core.getInput('version'));
-console.log(new Date().toLocaleDateString());
-
+const actor = github.context.actor;
+const version = github.context.ref.split('/').at(-1);
+const commits = github.context.payload.commits;
 const date = new Date().toLocaleDateString();
 
+const title = `${version} от ${date}`;
+
+const commitsString = commits.reduce((result, commit) => {
+  return (
+    result +
+    `<${commit.id}> <${commit.author.name}> <${commit.message}>
+  `
+  );
+}, '');
+
+const description = `ответственный за релиз ${actor}
+коммиты попавшие в релиз:
+${commitsString}`;
+
 const patchBody = JSON.stringify({
-  summary: `номер от ${date}`,
-  description: ` ответственный за релиз <имя того, кто запушил тег>
-     коммиты, попавшие в релиз:
-     <хеш коммита> <автор коммита> <описание коммита>
-     <хеш коммита> <автор коммита> <описание коммита>]
-     <хеш коммита> <автор коммита> <описание коммита>`,
+  summary: title,
+  description,
 });
 
 const options = {
@@ -63,37 +58,3 @@ req.write(patchBody);
 req.end();
 
 console.log('данные успешно записаны в трекер!');
-/*
-{
-  context: Context {
-    payload: {
-      after: '2edddbbb962e7ccb7b9b21e0cd2bfbd883e4676a',
-      base_ref: null,
-      before: '0000000000000000000000000000000000000000',
-      commits: [],
-      compare: 'https://github.com/irnq/infra-template/compare/v0.0.4-3',
-      created: true,
-      deleted: false,
-      forced: false,
-      head_commit: [Object],
-      pusher: [Object],
-      ref: 'refs/tags/v0.0.4-3',
-      repository: [Object],
-      sender: [Object]
-    },
-    eventName: 'push',
-    sha: '642d6aa0520b7ed4ea3692e32feecf229e9018fe',
-    ref: 'refs/tags/v0.0.4-3',
-    workflow: 'test and build on release',
-    action: '__run_2',
-    actor: 'irnq',
-    job: 'test-and-build',
-    runNumber: 45,
-    runId: 3331864653,
-    apiUrl: 'https://api.github.com',
-    serverUrl: 'https://github.com',
-    graphqlUrl: 'https://api.github.com/graphql'
-  },
-  getOctokit: [Function: getOctokit]
-}
-*/
