4f455f99f5abd56f5a10e8188d7bd3e1fcc5e9c0
irnq <sk1m1gm@yandex.ru>
2022-10-27 08:23:33 +0400

    вынес скрипт из yaml

--- gh-build.sh
+++ gh-build.sh
@@ -12,11 +12,20 @@ echo -e "$msg_error Ошибка при сборке проекта! $msg_common
 exit 1
 fi
 
-# echo "Добавим информацию в трекер..."
-# if `node ./util/PATCH.js`
-# then
-# echo -e "$msg_ok ОК: Информация внесена в трекер! $msg_common"
-# else
-# echo -e "$msg_error Произошла ошибка при обращении к трекеру! $msg_common"
-# exit 1
-# fi
+echo "устновим @actions/github:"
+if npm i @actions/github
+then
+echo -e "$msg_ok ОК: успешно! $msg_common"
+else
+echo -e "$msg_error Ошибка при установке @actions/github! $msg_common"
+exit 1
+fi
+
+echo "Добавим информацию в трекер..."
+if `node ./util/PATCH.js`
+then
+echo -e "$msg_ok ОК: Информация внесена в трекер! $msg_common"
+else
+echo -e "$msg_error Произошла ошибка при обращении к трекеру! $msg_common"
+exit 1
+fi
